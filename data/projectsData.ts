ae04de7b5876da966302484e7bf110e3e441eb42
Pony.Ma <mtf201013@gmail.com>
2023-09-24 17:04:51 +0800

    feat(blog): add new project Bidding Notice Parser

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,6 +5,12 @@ const projectsData = [
     imgSrc: '/static/images/nameko-apscheduler.png',
     href: 'https://github.com/ma-pony/nameko-apscheduler',
   },
+  {
+    title: 'Bidding Notice Parser',
+    description: '根据招标公告链接提取出招标公告的关键信息',
+    imgSrc: '/static/images/bidding-notice-parser',
+    href: 'https://bidding-parser.ponyma.io',
+  },
 ]
 
 export default projectsData
